e18dedd87fd0342f0e659a0361e669d514eccf77
chaanheeLEE <chanhee1519@gmail.com>
2025-07-21 11:34:42 +0900

    initial backend

---
+++ backend/src/test/java/first/webide/WebideApplicationTests.java
@@ -0,0 +1,13 @@
+package first.webide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
