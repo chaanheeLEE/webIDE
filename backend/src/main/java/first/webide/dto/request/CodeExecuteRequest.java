06ea64a927840ae983cfb3cbb012fb90321f03b2
chaanheeLEE <chanhee1519@gmail.com>
2025-07-26 01:53:16 +0900

    update: use PistonAPI, delete graalVM

--- backend/src/main/java/first/webide/dto/request/CodeExecuteRequest.java
+++ backend/src/main/java/first/webide/dto/request/CodeExecuteRequest.java
@@ -5,20 +5,25 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @NoArgsConstructor
 @Schema(description = "코드 실행 요청 DTO")
 public class CodeExecuteRequest {
 
-    @Schema(description = "실행할 코드", example = "System.out.println(\"Hello World\");")
-    @NotBlank(message = "코드가 입력되지 않았습니다.")
-    private String code;
-
     @Schema(description = "언어 (예: java, python)", example = "java")
     @NotBlank(message = "실행 가능한 언어가 선택되지 않았습니다.")
     private String language;
 
+    @Schema(description = "실행할 코드", example = "System.out.println(\"Hello World\");")
+    @NotBlank(message = "코드가 입력되지 않았습니다.")
+    private String code;
 
     @Schema(description = "파일 이름 (선택)", example = "Main.java", nullable = true)
     private String filename;
+
+    private String version;
+    private List<String> args;
+    private String input;
 }
