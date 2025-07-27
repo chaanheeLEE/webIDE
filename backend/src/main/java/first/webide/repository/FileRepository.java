41ee6a76a167bc943243b3782e59953841c2e794
chaanheeLEE <chanhee1519@gmail.com>
2025-07-27 18:33:06 +0900

    fix: children 정보를 항상 한 번에 불러와야 할 때를 고려

--- backend/src/main/java/first/webide/repository/FileRepository.java
+++ backend/src/main/java/first/webide/repository/FileRepository.java
@@ -2,6 +2,7 @@ package first.webide.repository;
 
 import first.webide.domain.FileNode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,6 +14,8 @@ public interface FileRepository extends JpaRepository<FileNode,Long> {
     Optional<FileNode> findByPath(String path);
 
     List<FileNode> findByParentOrderByTypeDescNameAsc(FileNode parent);
+
+    @Query("select distinct f from FileNode f left join fetch f.children where f.parent is null")
     Optional<FileNode> findByParentIsNull();
 
     boolean existsByParentAndName(FileNode parent, String name);
