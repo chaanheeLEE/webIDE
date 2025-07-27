cdc8cb1e39f50a62a777dd85af4d0a1f1522bd64
chaanheeLEE <chanhee1519@gmail.com>
2025-07-27 22:36:05 +0900

    refactor: extracts method to get files and adapt early return patter

--- backend/src/main/java/first/webide/service/CodeExecuteServiceImpl.java
+++ backend/src/main/java/first/webide/service/CodeExecuteServiceImpl.java
@@ -25,15 +25,10 @@ public class CodeExecuteServiceImpl implements CodeExecuteService {
 
         try {
             // 파일 이름 지정
-            String filename = request.getFilename();
-            if (filename == null || filename.isBlank()) {
-                filename = getDefaultFileName(request.getLanguage());
-            }
-            FileEntry fileEntry = new FileEntry(filename, request.getCode());
-            List<FileEntry> files = Collections.singletonList(fileEntry);
+            List<FileEntry> files = getFiles(request);
 
-            // Request 변환
-            PistonRequest pistonRequest = new  PistonRequest(
+            // Request 변환 -> 빌더패턴으로 리팩토링
+            PistonRequest pistonRequest = new PistonRequest(
                     request.getLanguage(), files, request.getArgs(),
                     request.getInput(), request.getVersion());
 
@@ -43,14 +38,15 @@ public class CodeExecuteServiceImpl implements CodeExecuteService {
             PistonResponse.RunResult run = response.getRun();
             long executionTime = System.currentTimeMillis() - startTime;
 
-            if (run.getCode() == 0){
-                return CodeExecuteResponse.success(run.getStdout(),  executionTime);
-            } else {
+            if (run.getCode() != 0){
                 String errMsg = run.getStderr() != null && !run.getStderr().isEmpty()
                         ? run.getStderr()
                         : "Unknown error (exit code " + run.getCode() + ")";
                 return CodeExecuteResponse.failure(errMsg,  executionTime);
             }
+
+            return CodeExecuteResponse.success(run.getStdout(),  executionTime);
+
         } catch (Exception e) {
             long executionTime = System.currentTimeMillis() - startTime;
             return CodeExecuteResponse.failure("ERROR = " + e.getMessage(),  executionTime);
@@ -59,6 +55,18 @@ public class CodeExecuteServiceImpl implements CodeExecuteService {
 
     }
 
+    private List<FileEntry> getFiles(CodeExecuteRequest request) {
+        String filename = request.getFilename();
+        if (filename == null || filename.isBlank()) {
+            filename = getDefaultFileName(request.getLanguage());
+        }
+        FileEntry fileEntry = new FileEntry(filename, request.getCode());
+        List<FileEntry> files = Collections.singletonList(fileEntry);
+        return files;
+    }
+
+
+    // 스트링보단 이넘타입으로 관리
     private String getDefaultFileName(String language) {
         return switch (language.toLowerCase()) {
             case "java" -> "Main.java";
