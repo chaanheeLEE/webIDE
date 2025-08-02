2ed7f1bbca5db8eca010f0ab998f9750d2050c48
chaanheeLEE <chanhee1519@gmail.com>
2025-08-02 22:48:57 +0900

    fix: add deleteByMember method

--- backend/src/main/java/first/webide/repository/RefreshTokenRepository.java
+++ backend/src/main/java/first/webide/repository/RefreshTokenRepository.java
@@ -1,5 +1,6 @@
 package first.webide.repository;
 
+import first.webide.domain.Member;
 import first.webide.domain.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,5 @@ import java.util.Optional;
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByMemberId(Long memberId);
     Optional<RefreshToken> findByToken(String token);
+    void deleteByMember(Member member);
 }
